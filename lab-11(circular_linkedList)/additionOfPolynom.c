25f7f96153648f4e680aec797ac7061503cca083
soham25122006 <syrathod25@gmail.com>
2026-07-07 22:19:24 +0530

    completed lab-11

--- lab-11(circular_linkedList)/additionOfPolynom.c
+++ lab-11(circular_linkedList)/additionOfPolynom.c
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
 struct Node
 {
     int coeff;
@@ -9,76 +12,148 @@ struct Node
     struct Node *link;
 };
 
-struct Node *createNode(int x, int y)
+struct Node *createNode(int coeff, int power)
 {
-    struct Node *newnode = malloc(sizeof(*newnode));
-    newnode->coeff = x;
-    newnode->power = y;
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->coeff = coeff;
+    newnode->power = power;
     newnode->link = NULL;
-
     return newnode;
 }
 
-void insert(struct Node **first, int x, int y)
+void insend(struct Node **first, int coeff, int power)
 {
-    struct Node *node = createNode(x, y);
+    struct Node *node = createNode(coeff, power);
 
     if (*first == NULL)
     {
-        node->link = node;
         *first = node;
         return;
     }
-    node->link = *first;
-    *first = node;
+
+    struct Node *temp = *first;
+    while (temp->link != NULL)
+    {
+        temp = temp->link;
+    }
+    temp->link = node;
 }
 
-void enterPolynomial(struct Node **first)
+void joinlists(struct Node **result, int coeff, int power)
 {
-    int x, i, y, num;
-    char more;
-    printf("enter total number of terms :=");
-    scanf("%d", &num);
-    for (i = 1; i <= num; i++)
-    {
-        printf("enter co-efficient of term num %d ", i);
-        scanf("%d", &x);
-        printf("enter power of term num %d ", i);
-        scanf("%d", &y);
-        insert(first, x, y);
-    }
-    printf("do you want to add any more terms in expression?(y/n)");
-    scanf("%c", &more);
-    if (more == 'y')
-    {
-        printf("how many? :-");
-        scanf("%d", &num);
+    struct Node *temp = *result;
 
-        for (i = 1; i <= num; i++)
+    while (temp != NULL)
+    {
+        if (temp->power == power)
         {
-            printf("enter co-efficient of term num %d ", i);
-            scanf("%d", &x);
-            printf("enter power of term num %d ", i);
-            scanf("%d", &y);
-            insert(first, x, y);
+            temp->coeff += coeff;
+            return;
         }
+        temp = temp->link;
     }
-    else
-    {}
+
+    insend(result, coeff, power);
 }
 
 char *addPolynomial(struct Node *first1, struct Node *first2)
 {
-    
-    return;
+    struct Node *result = NULL;
+    struct Node *temp = NULL;
+
+    temp = first1;
+    while (temp != NULL)
+    {
+        joinlists(&result, temp->coeff, temp->power);
+        temp = temp->link;
+    }
+
+    temp = first2;
+    while (temp != NULL)
+    {
+        joinlists(&result, temp->coeff, temp->power);
+        temp = temp->link;
+    }
+
+    char *str = malloc(1000 * sizeof(char));
+    if (str == NULL)
+    {
+        return NULL;
+    }
+
+    str[0] = '\0';
+    temp = result;
+    bool firstTerm = true;
+
+    while (temp != NULL)
+    {
+        if (temp->coeff != 0)
+        {
+            char term[100];
+            sprintf(term, "%d x^%d", temp->coeff, temp->power);
+
+            if (!firstTerm)
+            {
+                strcat(str, " + ");
+
+                firstTerm = false;
+            }
+
+            strcat(str, term);
+        }
+        temp = temp->link;
+    }
+
+    return str;
+}
+
+void displayPolynomial(struct Node *first)
+{
+    struct Node *temp = first;
+
+    while (temp != NULL)
+    {
+        printf("%d x^%d", temp->coeff, temp->power);
+        if (temp->link != NULL)
+            printf(" + ");
+        temp = temp->link;
+    }
+    printf("\n");
+}
+
+void inputPolynomial(struct Node **first)
+{
+    int num, i, coeff, power;
+
+    printf("ENTER TOTAL NUMBERS OF TERMS IN THIS EXPRESSION := ");
+    scanf("%d", &num);
+
+    for (i = 0; i < num; i++)
+    {
+        printf("enter (coeff) and (power of the variable) space separated := ");
+        scanf("%d", &coeff);
+        scanf("%d", &power);
+        insend(first, coeff, power);
+    }
 }
 
 void main()
 {
-    struct Node *first1 = NULL;
-    struct Node *first2 = NULL;
+    struct Node *list1 = NULL;
+    struct Node *list2 = NULL;
 
-    enterPolynomial(&first1);
-    enterPolynomial(&first2);
-    addPolynomial(first1, first2);
+    inputPolynomial(&list1);
+    inputPolynomial(&list2);
+
+    printf("Poly 1: ");
+    displayPolynomial(list1);
+
+    printf("Poly 2: ");
+    displayPolynomial(list2);
+
+    char *expression = addPolynomial(list1, list2);
+
+    printf("result is := %s\n", expression);
+
+    free(expression);
 }
